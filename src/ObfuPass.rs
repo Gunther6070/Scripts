3ff8eb01ff8a98fbdbb7cc9d938e2670021c7c08
Gunther6070 <haydenhartman10@yahoo.com>
2024-11-20 22:44:15 -0500

    Created ObfuPass file and modified Cargo dependencies

---
+++ src/ObfuPass.rs
@@ -0,0 +1,24 @@
+/**
+ * Filename: ObfuPass.rs
+ * Description: Automatically change a users password every five minutes, outputting a new encrypted password to file.
+ * Author: Hayden Hartman
+ */
+use std::process::{Command};
+extern crate whoami;
+
+
+// Function to change password
+// Function to encrypt password
+// Function to output the encrypted password to a file
+
+fn changePassword() -> String {
+
+    // let output = Command::new("net")
+    //     .arg("user")
+    //     .arg(whoami::username())
+    //     .arg("a")
+    //     .spawn()
+    //     .map_err(|e| format!("Failed to execute command: {}", e))?;
+    
+}
+
